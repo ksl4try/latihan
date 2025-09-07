505fbfb4c5a58c1f5311a1ea1e55d146dc73de53
ksl4try <ksl4try@gmail.com>
2025-09-07 18:02:42 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'latihan',
+  webDir: 'www'
+};
+
+export default config;
